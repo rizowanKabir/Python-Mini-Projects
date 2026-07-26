b3c7557ed247bf7d34614a3b0245bbfd24bf034f
rizowanKabir <sohagrizowan@gmail.com>
2026-07-26 10:31:23 +0600

    some due files

--- While_loops.py
+++ While_loops.py
@@ -27,4 +27,40 @@ number = 1
 while number <= 5:
     product *= number
     number += 1
-print(product)    
+print(product)  
+  
+# Reverse each word in a sentence
+
+sentence = input("Enter the sentence: ")
+words = sentence.split()
+
+for word in words:
+    i = len(word) - 1
+    while i >= 0:
+        print(word[i],end=" ")
+        i -= 1
+    print(end=" ")    
+
+# Count consonant of a string
+
+word = input("Enter the word to check: ")
+vowels = "aeiou"
+count = 0
+index = 0
+
+while index < len(word):
+    if word[index].lower() not in vowels and word[index].isalpha():
+        count += 1
+    index += 1
+
+print(f"Number of consonant is: {count}")       
+ 
+# Print the first 5 Multiples of 3
+
+multiple = 3
+count = 1
+
+while count <= 5:
+    print(multiple, end=" ")
+    multiple += 3
+    count += 1

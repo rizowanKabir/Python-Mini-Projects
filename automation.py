719e9780fed62cd35cb15d6f3509bfc41be91837
rizowanKabir <sohagrizowan@gmail.com>
2026-07-27 11:53:52 +0600

    just updated

--- automation.py
+++ automation.py
@@ -1,56 +1,57 @@
-#from openpyxl import Workbook,load_workbook
+from openpyxl import Workbook,load_workbook
 
 # Create Workbook
-#wb = Workbook()
+wb = Workbook()
 
 # Active Sheet
-# sheet = wb.active
-# sheet.title = "Selling Report"
+sheet = wb.active
+sheet.title = "Selling Report" 
 
 # Header
-#sheet.append(["Product", "Number", "Price", "Total"])
+sheet.append(["Product", "Number", "Price", "Total"])
 
 # Data
-# sell = [
-#     ["Shirt", 3, 2000],
-#     ["Pant", 4, 3000],
-#     ["T-shirts", 10, 3000],
-#]
+sell = [
+    ["Shirt", 3, 2000],
+    ["Pant", 4, 3000],
+    ["T-shirts", 10, 3000],
+]
 
-#total_sell = 0
+total_sell = 0
 
-# Calculate Total
-# for product, number, price in sell:
-#     total = number * price
-#     sheet.append([product, number, price, total])
-#     total_sell += total
+#Calculate Total
+
+for product, number, price in sell:
+    total = number * price
+    sheet.append([product, number, price, total])
+    total_sell += total
 
 # Grand Total
-#sheet.append(["", "", "Total Sell:", total_sell])
+sheet.append(["", "", "Total Sell:", total_sell])
 
 # Save Excel File & see how many rows
 
-# wb.save("sell_report.xlsx")
+wb.save("sell_report.xlsx")
 
-# print(f"Report Total Sell: {total_sell} Tk")
+print(f"Report Total Sell: {total_sell} Tk")
 
-# wb = load_workbook("sell_report.xlsx")
-# sheet = wb.active
+wb = load_workbook("sell_report.xlsx")
+sheet = wb.active
 
-# for row in sheet.iter_rows(values_only=True):
-#     print(row)
+for row in sheet.iter_rows(values_only=True):
+    print(row)
 
-#import os 
-#print(os.getcwd())
-#print(os.listdir())
+import os 
+print(os.getcwd())
+print(os.listdir())
 
-#from pathlib import Path
+from pathlib import Path
 
-# path = Path.cwd()
-# print(path)
+path = Path.cwd()
+print(path)
 
-# file = Path("sell_report.xlsx")
-# print(file.exists())
+file = Path("sell_report.xlsx") 
+print(file.exists())
 
 
 
